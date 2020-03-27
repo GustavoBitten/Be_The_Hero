507af77df1b134ef180054d772bcfa359758d2bc
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-03-26 22:33:43 -0300

    Finalizando o Backend

--- BackEnd/src/controllers/incidentController.js
+++ BackEnd/src/controllers/incidentController.js
@@ -11,10 +11,15 @@ let index = async (req, res) => {
     console.log(count)
 
     const incidents = await connection('incidents')
-
+    .join('ongs','ongs.id','=','incidents.ong_id')
     .limit(5)
     .offset((page -1)*5)
-    .select('*')
+    .select(['incidents.*',
+    'ongs.name',
+    'ongs.email',
+    'ongs.whatsapp',
+    'ongs.city',
+    'ongs.uf'])
 
     res.header('X-Total-Count', count['count(*)'] )
 
--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,18 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const route = require('./routes')
+
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(route)
-
-
-
-
-
-
-
-
 
 
 
