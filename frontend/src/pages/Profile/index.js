08c23efe6c8fb62e19e40ca8a6aaf38e4805dad6
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-04-19 04:47:17 -0300

    Finalizado o Front end web

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,84 +1,95 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import {FiPower,FiTrash2 } from 'react-icons/fi'
-import { Link } from "react-router-dom";
+import { Link,useHistory } from "react-router-dom";
+
+import api from '../../services/api'
+
 
 import logoImg from '../../assets/logo.svg'
+
 
 
 import './style.css'
 
 
 export default function Profile(){
+
+    const [incidents, setIncidents] = useState([])
+
+    const ongName = localStorage.getItem('ongName')
+    const ongId = localStorage.getItem('ongId')
+    const history = useHistory()
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId
+            }
+        }).then(response => {
+            setIncidents(response.data)
+        })
+    }, [ongId])
+
+
+
+
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incident/${id}`, {
+                headers: {
+                    Authorization: ongId
+                }
+            })
+
+            setIncidents(incidents.filter(incident=> incident.id !== id))
+
+        } catch (err) {
+            alert("Não foi possível deletar")
+            console.log(err)
+        }
+
+    }
+
+    function handleLogout(params) {
+        localStorage.clear()
+
+        history.push('/')
+        
+    }
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be the Hero"/>
-                <span>Bem vinda,APAD</span>
+                <span>Bem vinda,{ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button type="button" onClick={handleLogout}>
                     <FiPower size={18} color="#E02041"/>
 
                 </button>
             </header>
             <h1>Casos cadastrados</h1>
             <ul>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                {incidents.map(incident =>(
+                    <li key={incident.id}> 
+                        <strong>CASO:</strong>
+                    <p>{incident.title}</p>
 
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
+                        <strong>DESCRIÇÃO:</strong>
+                    <p>{incident.description }</p>
 
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
+                        <strong>VALOR:</strong>
+                    <p>{Intl.NumberFormat('pt-br',{style:'currency',currency:'BRL'}).format(incident.value)}</p>
 
-                    <button type="button">
+                    <button type="button" onClick={()=> handleDeleteIncident(incident.id) } >
                         <FiTrash2 size={20} color="#a8a8b3" />
                     </button>
                 </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
+                )
+                )
+                }
+                
+               
             </ul>
         </div>
         
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {FiArrowLeft } from 'react-icons/fi'
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+
+import api from "../../services/api"
 
 import logoImg from '../../assets/logo.svg'
 
@@ -9,6 +11,43 @@ import './style.css'
 
 
 export default function Register() {
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [whatsapp,setwhatsapp] = useState('')
+    const [city,setCity] = useState('')
+    const [uf,setUf] = useState('')
+
+    const history = useHistory()
+
+
+
+async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    }
+
+    try {
+
+        const response = await api.post('ongs', data)
+
+        alert('Deu certo, seu ID é: ' + response.data.id)
+
+        history.push('/')
+    } catch (err) {
+        alert('Erro no cadastro, aguarde para tentar novamente')
+    }
+
+
+}
+
+
+
     return ( 
         <div className = "register-container" >
             <div className = "content" >
@@ -22,13 +61,38 @@ export default function Register() {
                         Não tenho cadastro
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Nome da ONG"/>
-                    <input type="email" placeholder="E-mail"/>
-                    <input placeholder="WhatsApp"/>
+                <form onSubmit={handleRegister}>
+                    <input 
+                    value = {name}
+                    onChange = {e => setName(e.target.value)}
+                    placeholder="Nome da ONG"/>
+
+
+                    <input type="email" 
+                    value = {email}
+                    onChange = {e => setEmail(e.target.value)}
+                    placeholder="E-mail"/>
+
+
+                    <input 
+                    value = {whatsapp}
+                    onChange = {e => setwhatsapp(e.target.value)}
+                    placeholder="WhatsApp"/>
+
+
                 <div className="input-group">
-                    <input  placeholder="Cidade"/>
-                    <input  placeholder="UF" style={{width:80}}/>
+                    <input 
+                    value = {city}
+                    onChange = {e => setCity(e.target.value)}
+                    placeholder="Cidade"/>
+
+
+                    <input 
+                    value = {uf}
+                    onChange = {e => setUf(e.target.value)}
+                    placeholder="UF" style={{width:80}}/>
+
+
                 </div>
                 <button className="button" type="submit">Cadastrar</button>
 
